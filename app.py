5b210809ce28b7d00a4d55b03f2680ddfdbf59ae
ravensuresh-007 <ravensuresh@gmail.com>
2025-03-04 17:10:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ import os
 
 import Utilities as Ut, Market_Data_Utilities as Mdu, Ineractive_Data_Utilities as Idu
 
-print("Performing initial login...")
-Connect_Dict = Ut.Initial_Login_Modified("ATM011948")
-Market_Xt = Connect_Dict['Market_Xt']
-Interactive_Xt = Connect_Dict['Interactive_Xt']
-ClientID = Connect_Dict['ClientID']
-print("Login successful")
+
 
 app = Flask(__name__)
 
@@ -20,6 +15,14 @@ INSTRUMENTS = [
 ]
 
 @app.route('/')
+
+print("Performing initial login...")
+Connect_Dict = Ut.Initial_Login_Modified("ATM011948")
+Market_Xt = Connect_Dict['Market_Xt']
+Interactive_Xt = Connect_Dict['Interactive_Xt']
+ClientID = Connect_Dict['ClientID']
+print("Login successful")
+
 def home():
     return render_template('index.html')
 
